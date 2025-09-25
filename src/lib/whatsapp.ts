639769df3251e8a60228b01596f7ba82bc238975
João Victor Gadelha <joaovictor_gadelha@live.com>
2025-09-24 21:21:26 -0300

    feat: reintroduce database schema and relations for conversations and messages

--- src/lib/whatsapp.ts
+++ src/lib/whatsapp.ts
@@ -1,26 +1,11 @@
 import pkg from "whatsapp-web.js";
 import qrcode from "qrcode-terminal";
-import Groq from "groq-sdk";
 
 const { Client, LocalAuth } = pkg;
 
 export const whatsAppClient = new Client({
   authStrategy: new LocalAuth(),
 });
-
-const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
-
-export async function getGroqChatCompletion(message: string) {
-  return groq.chat.completions.create({
-    messages: [
-      {
-        role: "user",
-        content: message,
-      },
-    ],
-    model: "llama-3.3-70b-versatile",
-  });
-}
 
 whatsAppClient.once("ready", () => {
   console.log("Client is ready!");
@@ -30,6 +15,9 @@ whatsAppClient.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
 });
 
-whatsAppClient.on("message_create", async (message) => {});
+whatsAppClient.on("message_create", async (message) => {
+  if (message.fromMe && message.body.startsWith("!")) {
+  }
+});
 
 whatsAppClient.initialize();
