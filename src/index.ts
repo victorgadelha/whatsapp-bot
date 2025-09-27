74ac9dd55023891373625a596747afdd57a1e64b
João Victor Gadelha <joaovictor_gadelha@live.com>
2025-09-26 22:02:57 -0300

    feat: update database connection to use TURSO environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { drizzle } from "drizzle-orm/libsql";
 
-const db = drizzle({
+export const db = drizzle({
   connection: {
-    url: process.env.DATABASE_URL!,
-    authToken: process.env.DATABASE_AUTH_TOKEN,
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN,
   },
 });
